7c10a4c8e5f9080171599d049a8e44e4f23cfec2
Shubhamrandale <shubhamrandale8@gmail.com>
2024-08-18 19:12:55 +0530

    Created OTPUtility class

---
+++ src/main/java/com/BankApplication/utils/OTPUtility.java
@@ -0,0 +1,15 @@
+package com.BankApplication.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OTPUtility {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    public Integer sendOTP(){
+        logger.info("inside sendOTP() method of {}",this.getClass());
+        return 123456;
+    }
+
+}
