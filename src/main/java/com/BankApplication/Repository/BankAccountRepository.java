20370664bd6eff36e3abad908377d6d35dd2cb27
Shubhamrandale <shubhamrandale8@gmail.com>
2024-08-20 17:05:42 +0530

    Link account method created without generating otp

--- src/main/java/com/BankApplication/Repository/BankAccountRepository.java
+++ src/main/java/com/BankApplication/Repository/BankAccountRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 public interface BankAccountRepository extends JpaRepository<BankAccountEntity, Long> {
     List<BankAccountEntity> findByMobileNo(Long mobileNo);
+
+    Optional<BankAccountEntity> findByMobileNoAndAccountNo(Long mobileNo, Long accountNo);
+
 }
--- src/main/java/com/BankApplication/Service/impl/DigitalBankAccountServiceImpl.java
+++ src/main/java/com/BankApplication/Service/impl/DigitalBankAccountServiceImpl.java
@@ -1,12 +1,43 @@
 package com.BankApplication.Service.impl;
 
+import com.BankApplication.DTO.DigitalBankAccountDto;
+import com.BankApplication.Entities.BankAccountEntity;
+import com.BankApplication.Entities.DigitalBankAccountEntity;
+import com.BankApplication.Exception.DigitalBankingException;
+import com.BankApplication.Repository.BankAccountRepository;
+import com.BankApplication.Repository.DigitalBankAccountRepository;
 import com.BankApplication.Service.DigitalBankAccountService;
+import com.BankApplication.utils.OTPUtility;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DigitalBankAccountServiceImpl implements DigitalBankAccountService
 {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+
+    @Autowired
+    private DigitalBankAccountRepository digitalBankAccountRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private OTPUtility otpUtility;
+
     @Override
     public String linkAccount(Long mobileNo, Long accountNo) {
-        return null;
+
+        BankAccountEntity bankAccount = bankAccountRepository.findByMobileNoAndAccountNo(mobileNo, accountNo).
+                orElseThrow(() -> new DigitalBankingException("NO ACCOUNT FOUND"));
+        DigitalBankAccountEntity digitalBankAccount = new DigitalBankAccountEntity();
+        digitalBankAccount.setMobileNo(mobileNo);
+        digitalBankAccount.setBankAccount(bankAccount);
+        digitalBankAccount.setAccountType(bankAccount.getBankType());
+
+        digitalBankAccountRepository.save(digitalBankAccount);
+
+        return "Account Linked Successfully. Digital Banking Id :- "+digitalBankAccount.getDigitalBankingId();
     }
 
     @Override
@@ -18,4 +49,16 @@ public class DigitalBankAccountServiceImpl implements DigitalBankAccountService
     public Double checkBalance(Long mobileNo, Long accountNo) {
         return null;
     }
+
+    public DigitalBankAccountDto mapToDto(DigitalBankAccountEntity digitalBankAccountEntity){
+        DigitalBankAccountDto digitalBankAccountDto = mapper.map(digitalBankAccountEntity, DigitalBankAccountDto.class);
+        return digitalBankAccountDto;
+    }
+
+    public DigitalBankAccountEntity mapToEntity(DigitalBankAccountDto digitalBankAccountDto){
+        DigitalBankAccountEntity digitalBankAccountEntity = mapper.map(digitalBankAccountDto,DigitalBankAccountEntity.class);
+        return digitalBankAccountEntity;
+    }
+
+
 }
